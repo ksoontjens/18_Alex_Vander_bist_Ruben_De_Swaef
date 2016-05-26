7341197fc960c8b4e970482675ace360c7f9909b
rubends <rubendeswaef@gmail.com>
2016-05-26 19:11:57 +0200

    background

--- Pong/src/hellotvxlet/HelloTVXlet.java
+++ Pong/src/hellotvxlet/HelloTVXlet.java
@@ -32,7 +32,7 @@ public class HelloTVXlet implements Xlet, UserEventListener, HBackgroundImageLis
     private HBackgroundConfigTemplate bgTemplate;
     private HStillImageBackgroundConfiguration bgConfiguration;
     private HBackgroundDevice bgDevice;
-    private HBackgroundImage background=new HBackgroundImage("back1.m2v");
+    private HBackgroundImage background=new HBackgroundImage("back3.m2v");
     
     public void destroyXlet(boolean unconditional) throws XletStateChangeException {
         background.flush();
